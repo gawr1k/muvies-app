16f0b41870aa51db1b6ce9d5b5c560ad956cfc1a
Гасим Мурасов <gasimmurasov@gmail.com>
2023-12-13 18:28:29 +0500

    started fixing the edit list

--- src/components/ApiClient/ApiClient.jsx
+++ src/components/ApiClient/ApiClient.jsx
@@ -1,22 +1,22 @@
-import Access_key from "./api-key"
-
-
+import Access_key from "./api-key";
 
 const ApiClient = (search, page) => {
-
   const options = {
-    method: 'GET',
+    method: "GET",
     headers: {
-      accept: 'application/json',
-      Authorization: Access_key
-    }
+      accept: "application/json",
+      Authorization: Access_key,
+    },
   };
-  
-  const result = fetch(`https://api.themoviedb.org/3/search/movie?query=${search}&page=${page}`, options)
-    .then(response => response.json())
-    .then(response => response)
-    .catch(err => console.error(err));
-    return result;
-}
-  
-  export default ApiClient;
+
+  const result = fetch(
+    `https://api.themoviedb.org/3/search/movie?query=${search}&page=${page}`,
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => response)
+    .catch((err) => console.error(err));
+  return result;
+};
+
+export default ApiClient;
--- src/components/Card/genre_id.js
+++ src/components/Card/genre_id.js
@@ -50,90 +50,90 @@
 // export default GenreId;
 
 const genresData = {
-  "genres": [
+  genres: [
     {
-      "id": 28,
-      "name": "Action"
+      id: 28,
+      name: "Action",
     },
     {
-      "id": 12,
-      "name": "Abenteuer"
+      id: 12,
+      name: "Abenteuer",
     },
     {
-      "id": 16,
-      "name": "Animation"
+      id: 16,
+      name: "Animation",
     },
     {
-      "id": 35,
-      "name": "Komödie"
+      id: 35,
+      name: "Komödie",
     },
     {
-      "id": 80,
-      "name": "Krimi"
+      id: 80,
+      name: "Krimi",
     },
     {
-      "id": 99,
-      "name": "Dokumentarfilm"
+      id: 99,
+      name: "Dokumentarfilm",
     },
     {
-      "id": 18,
-      "name": "Drama"
+      id: 18,
+      name: "Drama",
     },
     {
-      "id": 10751,
-      "name": "Familie"
+      id: 10751,
+      name: "Familie",
     },
     {
-      "id": 14,
-      "name": "Fantasy"
+      id: 14,
+      name: "Fantasy",
     },
     {
-      "id": 36,
-      "name": "Historie"
+      id: 36,
+      name: "Historie",
     },
     {
-      "id": 27,
-      "name": "Horror"
+      id: 27,
+      name: "Horror",
     },
     {
-      "id": 10402,
-      "name": "Musik"
+      id: 10402,
+      name: "Musik",
     },
     {
-      "id": 9648,
-      "name": "Mystery"
+      id: 9648,
+      name: "Mystery",
     },
     {
-      "id": 10749,
-      "name": "Liebesfilm"
+      id: 10749,
+      name: "Liebesfilm",
     },
     {
-      "id": 878,
-      "name": "Science Fiction"
+      id: 878,
+      name: "Science Fiction",
     },
     {
-      "id": 10770,
-      "name": "TV-Film"
+      id: 10770,
+      name: "TV-Film",
     },
     {
-      "id": 53,
-      "name": "Thriller"
+      id: 53,
+      name: "Thriller",
     },
     {
-      "id": 10752,
-      "name": "Kriegsfilm"
+      id: 10752,
+      name: "Kriegsfilm",
     },
     {
-      "id": 37,
-      "name": "Western"
-    }
-  ]
+      id: 37,
+      name: "Western",
+    },
+  ],
 };
 
 const getGenreNamesByIds = (genreIds) => {
   return genreIds.map((id) => {
     const genre = genresData.genres.find((g) => g.id === id);
-    return genre ? genre.name : 'Unknown';
+    return genre ? genre.name : "Unknown";
   });
 };
 
@@ -142,5 +142,4 @@ const exampleGenreIds = [28, 12, 16];
 const genreNames = getGenreNamesByIds(exampleGenreIds);
 console.log(genreNames); // Выведет: ["Action", "Abenteuer", "Animation"]
 
-
-export default getGenreNamesByIds
+export default getGenreNamesByIds;
